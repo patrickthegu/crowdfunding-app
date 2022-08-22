5baaab97ce7a2c6dfb2269f6dd559c8f93db62ab
Artur0705 <artlil420@gmail.com>
2022-08-22 21:37:08 +1000

    Required additional modules

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const path = require('path');
+
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -23,8 +27,15 @@ const sess = {
 
 app.use(session(sess));
 
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(routes);
 
